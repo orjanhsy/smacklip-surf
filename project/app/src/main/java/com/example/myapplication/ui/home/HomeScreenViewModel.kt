2a8b6a249d7edfeb972dc5b15203b511a64bb7d5
Tobias Windingstad <tobiawi@ulrik.uio.no>
2024-03-14 17:06:59 +0100

    change type of homeScreen UI state variables

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
@@ -4,9 +4,9 @@ import com.example.myapplication.model.metalerts.Features
 
 data class HomeScreenUiState(
     val name : String,
-    val windSpeed : Double,
-    val windGust : Double,
-    val windDirection : String,
+    val windSpeed : List<Pair<String, Double>>,
+    val windGust : List<Pair<String, Double>>,
+    val windDirection : List<Pair<String, Double>>,
     val waveHeight : List<Pair<String, Double>>,
     val relevantAlerts : List<Features>
 )
