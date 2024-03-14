6e4bb31a2f4669ecbd207cd055581ad6694f3ecc
Tobias Windingstad <tobiawi@ulrik.uio.no>
2024-03-14 17:03:48 +0100

    add homeScreen ui state to homeScreen ui model

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.myapplication.ui.home
 
+import com.example.myapplication.model.metalerts.Features
+
+data class HomeScreenUiState(
+    val name : String,
+    val windSpeed : Double,
+    val windGust : Double,
+    val windDirection : String,
+    val waveHeight : List<Pair<String, Double>>,
+    val relevantAlerts : List<Features>
+)
 class HomeScreenViewModel(
 
 ) {
