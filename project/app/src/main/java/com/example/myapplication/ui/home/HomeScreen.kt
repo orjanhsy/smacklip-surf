b0dfc19dcb00a593030c019b7e3d0bd64e500ad5
Agnes <agnes.birkemo@hotmail.com>
2024-03-15 13:14:27 +0100

    add lazy column to homescreen and hoddevik image to recource manager

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreen.kt
@@ -1,24 +1,71 @@
 package com.example.myapplication.ui.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.model.SurfArea
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
-    SurfAreaCard()
+fun HomeScreen(homeScreenViewModel : HomeScreenViewModel = viewModel()) {
+
+    //SurfAreaCard()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "Locations")
+                })
+        })
+
+
+    { innerPadding ->
+
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(SurfArea.entries) { party ->
+                SurfAreaCard()
+            }
+
+        }
+    }
 }
+
+
 
 @Composable
 fun SurfAreaCard() {
@@ -27,39 +74,55 @@ fun SurfAreaCard() {
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .padding(16.dp)
-            ,
-            horizontalAlignment = Alignment.Start,
-        ) {
-            Row {
-                Text(
-                    text = "Hoddevik",
-                    fontWeight = FontWeight.Bold
-                )
-            }
-            Row {
-                Text(
-                    text = "Wind"
-                )
-            }
 
-            Row {
-                Text(
-                    text = "Wave height"
-                )
+        Row(
+            //contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Row {
+                    Text(
+                        text = "Hoddevik",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+                Row {
+                    Text(
+                        text = "Wind"
+                    )
+                }
+
+                Row {
+                    Text(
+                        text = "Wave height"
+                    )
+                }
             }
-        }
-        Column {
-            Text("hei")
+            Column (
+                horizontalAlignment = Alignment.End
+            ){
+                Text("hei")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 private fun PreviewSurfAreaCard() {
     SurfAreaCard()
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHomeScreen() {
+    HomeScreen()
+}
+
