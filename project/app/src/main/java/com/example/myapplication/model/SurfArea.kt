688f9f13f5d700619a661cf39c6c7fa5dd78addc
munaai <munaai@ifi.uio.no>
2024-03-15 14:52:38 +0100

    add locationName to surfArea

--- project/app/src/main/java/com/example/myapplication/model/SurfArea.kt
+++ project/app/src/main/java/com/example/myapplication/model/SurfArea.kt
@@ -4,11 +4,12 @@ import com.example.myapplication.R
 
 
 enum class SurfArea(
+    val locationName: String,
     val lat: Double,
     val lon: Double,
     val image: Int
 ) {
-    HODDEVIK(62.723, 5.103, R.drawable.cover___hoddevik),
-    NORDKAPP(71.1655, 25.7992, 0),
-    FEDJE(60.7789, 4.71486, 0)
+    HODDEVIK("Hoddevik",62.723, 5.103, R.drawable.cover___hoddevik),
+    NORDKAPP("Nordkapp",71.1655, 25.7992, 0),
+    FEDJE("Fedje",60.7789, 4.71486, 0)
 }
