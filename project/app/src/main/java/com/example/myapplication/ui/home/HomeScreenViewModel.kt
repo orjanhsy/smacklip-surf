407d5a9ecb9bd5eb048a822ac5630fb94f1331d3
munaai <munaai@ifi.uio.no>
2024-03-15 14:32:08 +0100

    resolved merge conflicts

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
@@ -1,16 +1,24 @@
 package com.example.myapplication.ui.home
-
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.data.smackLip.SmackLipRepositoryImpl
 import com.example.myapplication.model.metalerts.Features
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 data class HomeScreenUiState(
-    val locationName : String,
-    val windSpeed : List<Pair<String, Double>>,
-    val windGust : List<Pair<String, Double>>,
-    val windDirection : List<Pair<String, Double>>,
-    val waveHeight : List<Pair<String, Double>>,
-    val relevantAlerts : List<Features>
+    val locationName : String = "",
+    val windSpeed : List<Pair<String, Double>> = emptyList(),
+    val windGust : List<Pair<String, Double>> = emptyList(),
+    val windDirection : List<Pair<String, Double>> = emptyList(),
+    val waveHeight : List<Pair<String, Double>> = emptyList(),
+    val relevantAlerts : List<Features> = emptyList()
 )
 class HomeScreenViewModel : ViewModel() {
+    private val smackLipRepository = SmackLipRepositoryImpl()
+    private val _homeScreenUiState = MutableStateFlow(HomeScreenUiState())
+    val homeScreenUiState: StateFlow<HomeScreenUiState> = _homeScreenUiState.asStateFlow()
+
+
 
 }
