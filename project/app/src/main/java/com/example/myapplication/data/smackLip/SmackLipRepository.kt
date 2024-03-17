1c4e0b2fd7a23b8fba24937873709b17280a3db1
orjanhsy <orjanhsy@uio.no>
2024-03-17 13:59:32 +0100

    remove  'allFeatures' parameter from getRelevantAlertsFor and make it suspend
    now makes an api call by 'getFeatures()' if features is empty
    changed 'getRelevantAlertsFor()' in SLRepo to fit the changes
    changed tests in exampleUnitTest to fit the changes

--- project/app/src/main/java/com/example/myapplication/data/smackLip/SmackLipRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/smackLip/SmackLipRepository.kt
@@ -16,7 +16,7 @@ import com.example.myapplication.model.oceanforecast.DataOF
 
 
 interface SmackLipRepository {
-    fun getRelevantAlertsFor(surfArea: SurfArea, allFeatures: List<Features>): List<Features>
+    suspend fun getRelevantAlertsFor(surfArea: SurfArea): List<Features>
     fun getWaveHeights(timeSeries: List<Pair<String, DataOF>>): List<Pair<String, Double>>
     suspend fun getTimeSeriesOF(): List<Pair<String, DataOF>>
     suspend fun getTimeSeriesLF(): List<Pair<String, DataLF>>
@@ -32,9 +32,9 @@ class SmackLipRepositoryImpl (
 
     ): SmackLipRepository {
 
-        //MET
-    override fun getRelevantAlertsFor(surfArea: SurfArea, allFeatures: List<Features>): List<Features> {
-        return metAlertsRepository.getRelevantAlertsFor(surfArea, allFeatures)
+    //MET
+    override suspend fun getRelevantAlertsFor(surfArea: SurfArea): List<Features> {
+        return metAlertsRepository.getRelevantAlertsFor(surfArea)
      }
 
     //OF
