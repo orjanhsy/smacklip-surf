3ebc1a06bbe45b8c29c3ff7ee85bc94fd25ce96c
Ørjan Hammer Sylta <orjanhsy@ulrik.uio.no>
2024-03-18 13:42:24 +0100

    Feature/add alerts to surfareaviewmodel (#49)
    * add function to update alerts in 'SurfAreaViewModel'
    
    * changed location to be a surfarea
    
    ---------
    
    Co-authored-by: orjanhsy <orjanhsy@uio.no>
    Co-authored-by: juliesmac <julikal@uio.no>
    Co-authored-by: orjanhsy <orjanhsy@gmail.com>
    Co-authored-by: Tobias Windingstad <tobiawi@ulrik.uio.no>
    Co-authored-by: Agnes <agnes.birkemo@hotmail.com>
    Co-authored-by: Muna Isman <munaisman@eduroam-193-157-214-56.wlan.uio.no>
    Co-authored-by: Muna Abdirisak Isman <munaai@ulrik.uio.no>

--- project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
@@ -1,25 +1,35 @@
 package com.example.myapplication.ui.surfarea
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myapplication.data.smackLip.SmackLipRepositoryImpl
+import com.example.myapplication.model.SurfArea
 import com.example.myapplication.model.metalerts.Features
 import com.example.myapplication.ui.home.HomeScreenUiState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class SurfAreaScreenUiState(
-    val locationName : String = "",
-    // val windSpeedNext7Days:,
-    // val waveHeightNext7Days:,
-    // val alerts: List<List<Features>>,
-    // val windGustNext7Days:,
-    // val alt mulig next 7 days om er relevant
+    val location: SurfArea? = null,
+    val alerts: List<Features> = emptyList()
 
 )
+
 class SurfAreaScreenViewModel: ViewModel() {
     private val smackLipRepository = SmackLipRepositoryImpl()
     private val _surfAreaScreenUiState = MutableStateFlow(SurfAreaScreenUiState())
     val surfAreaScreenUiState: StateFlow<SurfAreaScreenUiState> = _surfAreaScreenUiState.asStateFlow()
 
+    fun updateAlerts() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _surfAreaScreenUiState.update {
+                val newAlerts = if (it.location != null) smackLipRepository.getRelevantAlertsFor(it.location) else listOf()
+                it.copy(alerts = newAlerts)
+            }
+        }
+    }
 }
