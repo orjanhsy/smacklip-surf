eacdaf413dcf480ce48b34197793b1f842752de8
Agnes <agnes.birkemo@hotmail.com>
2024-03-18 17:37:09 +0100

    use getTimeListFromTimeString in getWindSpeedOfGust, add test too, test ok

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreenViewModel.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.launch
 data class HomeScreenUiState(
     val locationName: String = "",
     val windSpeed: List<Pair<List<Int>, Double>> = emptyList(),
-    val windGust: List<Pair<String, Double>> = emptyList(),
+    val windGust: List<Pair<List<Int>, Double>> = emptyList(),
     val windDirection: List<Pair<String, Double>> = emptyList(),
     val waveHeight: List<Pair<List<Int>, Double>> = emptyList(),
     val allRelevantAlerts: List<List<Features>> = emptyList()
