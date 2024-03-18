a688373ca8fdcce82648b1cb593dcb84d632b55d
orjanhsy <orjanhsy@gmail.com>
2024-03-18 11:57:36 +0100

    forslag til hvordan vi strukturerer 'SurfAreaScreen'

--- project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreen.kt
@@ -1,5 +1,25 @@
 package com.example.myapplication.ui.surfarea
 
-class SurfAreaScreen {
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+@Composable
+fun SurfAreaScreen(surfAreaScreenViewModel: SurfAreaScreenViewModel = viewModel()) {
+    val surfAreaScreenUiState: SurfAreaScreenUiState by surfAreaScreenViewModel.surfAreaScreenUiState.collectAsState()
+
+}
+
+
+//forslag
+@Composable
+fun ShowForecastNext24hrs(day: String) {
+    // vis 0-6, 6-12, 12-18, 18-24
+}
+
+@Composable
+fun ShowForecastNext7Days() {
+    val days = listOf<String>("Monday", "Tuesday")
+    days.forEach { ShowForecastNext24hrs(day = it) }
 }
