f79fdb023f647b6adc22c4cfdfae63ebefc46314
Ørjan Hammer Sylta <orjanhsy@ulrik.uio.no>
2024-03-18 13:12:44 +0100

    Add initial viewmodel and screen for surf areas (#48)
    * add empty surf screen and view model
    
    * add 'SurfAreaScreenUiState'
    
    * describe needed variables in 'SurfAreaScreenUiState'
    
    * forslag til hvordan vi strukturerer 'SurfAreaScreen'
    
    * add method signatures for ocean forecast data to SurfAreaScreenViewModel
    
    * remove empty functions
    
    ---------
    
    Co-authored-by: orjanhsy <orjanhsy@uio.no>
    Co-authored-by: juliesmac <julikal@uio.no>
    Co-authored-by: orjanhsy <orjanhsy@gmail.com>
    Co-authored-by: Tobias Windingstad <tobiawi@ulrik.uio.no>
    Co-authored-by: Agnes <agnes.birkemo@hotmail.com>
    Co-authored-by: Muna Isman <munaisman@eduroam-193-157-214-56.wlan.uio.no>
    Co-authored-by: Muna Abdirisak Isman <munaai@ulrik.uio.no>

---
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.ui.surfarea
+
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.data.smackLip.SmackLipRepositoryImpl
+import com.example.myapplication.model.metalerts.Features
+import com.example.myapplication.ui.home.HomeScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class SurfAreaScreenUiState(
+    val locationName : String = "",
+    // val windSpeedNext7Days:,
+    // val waveHeightNext7Days:,
+    // val alerts: List<List<Features>>,
+    // val windGustNext7Days:,
+    // val alt mulig next 7 days om er relevant
+
+)
+class SurfAreaScreenViewModel: ViewModel() {
+    private val smackLipRepository = SmackLipRepositoryImpl()
+    private val _surfAreaScreenUiState = MutableStateFlow(SurfAreaScreenUiState())
+    val surfAreaScreenUiState: StateFlow<SurfAreaScreenUiState> = _surfAreaScreenUiState.asStateFlow()
+
+}
