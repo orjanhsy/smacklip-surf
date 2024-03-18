d6d399d98c7a6f66650f33f08185b5589da17a0d
orjanhsy <orjanhsy@gmail.com>
2024-03-18 11:19:02 +0100

    add 'SurfAreaScreenUiState'

--- project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.myapplication.ui.surfarea
 
+import com.example.myapplication.model.metalerts.Features
+
+data class SurfAreaScreenUiState(
+    val locationName : String = "",
+    val windSpeed : List<Pair<String, Double>> = emptyList(),
+    val windGust : List<Pair<String, Double>> = emptyList(),
+    val windDirection : List<Pair<String, Double>> = emptyList(),
+    val waveHeight : List<Pair<String, Double>> = emptyList(),
+    val allRelevantAlerts : List<List<Features>> = emptyList()
+)
 class SurfAreaScreenViewModel {
 }
