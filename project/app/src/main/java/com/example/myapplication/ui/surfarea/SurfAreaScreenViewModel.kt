97c8a9e5bb58c701c703b98404241545357df7ec
orjanhsy <orjanhsy@gmail.com>
2024-03-18 11:42:16 +0100

    describe needed variables in 'SurfAreaScreenUiState'

--- project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreenViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.myapplication.ui.surfarea
 
+import com.example.myapplication.data.smackLip.SmackLipRepositoryImpl
 import com.example.myapplication.model.metalerts.Features
+import com.example.myapplication.ui.home.HomeScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 data class SurfAreaScreenUiState(
     val locationName : String = "",
-    val windSpeed : List<Pair<String, Double>> = emptyList(),
-    val windGust : List<Pair<String, Double>> = emptyList(),
-    val windDirection : List<Pair<String, Double>> = emptyList(),
-    val waveHeight : List<Pair<String, Double>> = emptyList(),
-    val allRelevantAlerts : List<List<Features>> = emptyList()
+    // val windSpeedNext7Days:,
+    // val waveHeightNext7Days:,
+    // val alerts: List<List<Features>>,
+    // val windGustNext7Days:,
+    // val alt mulig next 7 days om er relevant
+
 )
 class SurfAreaScreenViewModel {
+    private val smackLipRepository = SmackLipRepositoryImpl()
+    private val _surfAreaScreenUiState = MutableStateFlow(SurfAreaScreenUiState())
+    val surfAreaScreenUiState: StateFlow<SurfAreaScreenUiState> = _surfAreaScreenUiState.asStateFlow()
 }
