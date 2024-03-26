ce79aeafe9af118ce88ec0cff2d84042643680c1
Agnes <agnes.birkemo@hotmail.com>
2024-03-26 09:49:08 +0100

    add dependencies settings.gradle.kts

--- project/settings.gradle.kts
+++ project/settings.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.jsoup.safety.Safelist.basic
+
 pluginManagement {
     repositories {
         google()
@@ -10,6 +12,17 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        // Mapbox Maven repository
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            // Do not change the username below. It should always be "mapbox" (not your username).
+            credentials.username = "mapbox"
+            // Use the secret token stored in gradle.properties as the password
+            credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            authentication.create<BasicAuthentication>("basic")
+        }
+
     }
 }
 
