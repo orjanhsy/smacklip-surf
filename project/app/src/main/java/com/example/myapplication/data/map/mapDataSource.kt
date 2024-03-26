41c5fdef040862fa363472f917296073de45764c
Agnes <agnes.birkemo@hotmail.com>
2024-03-26 09:32:12 +0100

    add secret access token for mapbox to gradle.properties

--- project/app/src/main/java/com/example/myapplication/data/map/mapDataSource.kt
+++ project/app/src/main/java/com/example/myapplication/data/map/mapDataSource.kt
@@ -2,9 +2,12 @@ package com.example.myapplication.data.map
 
 import com.example.myapplication.data.helpers.HTTPServiceHandler
 import com.example.myapplication.data.helpers.HTTPServiceHandler.NORWAY_URL
+import com.example.myapplication.model.oceanforecast.OceanForecast
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
 import io.ktor.util.appendIfNameAbsent
 
