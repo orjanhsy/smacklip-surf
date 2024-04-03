d3efdf38a2051688fac4961728072d0214217432
Muna Isman <munaisman@Munas-MacBook-Pro.local>
2024-04-03 02:43:25 +0200

    added log for datasource

--- project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastDataSource.kt
+++ project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastDataSource.kt
@@ -19,6 +19,7 @@ class LocationForecastDataSource(
     private val path: String = LOCATION_FORECAST_URL
 
 ) {
+    private val TAG = "LocationForecastDataSource"
 
 
     private val client = HttpClient(){
@@ -30,8 +31,19 @@ class LocationForecastDataSource(
             gson()
         }
     }
-    suspend fun fetchLocationForecastData(surfArea: SurfArea): LocationForecast {
-        val locationForecast: LocationForecast = client.get("$path?lat=${surfArea.lat}&lon=${surfArea.lon}").body()
+    suspend fun fetchLocationForecastData(surfArea: SurfArea): LocationForecast? {
+        Log.d(TAG, "Fetching forecast data for SurfArea: ${surfArea.locationName}, lat: ${surfArea.lat}, lon: ${surfArea.lon}")
+        val locationUrl = "$path?lat=${surfArea.lat}&lon=${surfArea.lon}"
+        val locationForecast: LocationForecast? = client.get(locationUrl).body()
+        Log.d(TAG, "Raw Response: $locationForecast" )
+        Log.d(TAG, "Response for ${surfArea.locationName} forecast: $locationForecast")
+        val windDirection = locationForecast?.properties?.timeseries?.firstOrNull()?.data?.instant?.details?.wind_from_direction
+        val windSpeed = locationForecast?.properties?.timeseries?.firstOrNull()?.data?.instant?.details?.wind_speed
+        val windSpeedOfGust = locationForecast?.properties?.timeseries?.firstOrNull()?.data?.instant?.details?.wind_speed_of_gust
+
+        Log.d(TAG, "Wind Direction: $windDirection")
+        Log.d(TAG, "Wind Speed: $windSpeed")
+        Log.d(TAG, "Wind Speed of Gust: $windSpeedOfGust")
         return locationForecast
     }
 }
