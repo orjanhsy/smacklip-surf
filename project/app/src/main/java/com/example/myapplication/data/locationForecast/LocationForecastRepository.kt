b93b38fe6e7c7ecab368630e4c237b919059da0b
Muna Isman <munaisman@Munas-MacBook-Pro.local>
2024-04-04 06:31:49 +0200

    made both OF and LF api call work for all surfareas

--- project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastRepository.kt
@@ -14,41 +14,56 @@ interface LocationForecastRepository {
 }
 
 class LocationForecastRepositoryImpl(
-    private val locationForecastDataSource: LocationForecastDataSource = LocationForecastDataSource(path = HTTPServiceHandler.OCEAN_FORECAST_URL)
+    private val locationForecastDataSource: LocationForecastDataSource = LocationForecastDataSource()
 ): LocationForecastRepository {
 
     override suspend fun getTimeSeries(surfArea: SurfArea): List<Pair<String, DataLF>> {
-        val locationForecast = locationForecastDataSource.fetchLocationForecastData(surfArea)
-        Log.d("LocationTimeSeries", "Location forecast for ${surfArea.locationName}: $locationForecast" )
-        return if(locationForecast?.properties?.timeseries != null) {
-            locationForecast.properties.timeseries.map { it.time to it.data }
-        } else {
-            emptyList()
-        }
+        val timeSeries : List<TimeserieLF> = locationForecastDataSource.fetchLocationForecastData(surfArea).properties.timeseries
+        return timeSeries.map { it.time to it.data }
     }
 
-    private suspend fun getWindData(surfArea: SurfArea, getData: (DataLF) -> Double): List<Pair<String, Double>> {
-        val timeSeries = getTimeSeries(surfArea)
-        return timeSeries.map { it.first to getData(it.second) }
+    private fun findWindSpeedFromData(dataLF: DataLF): Double{
+        return dataLF.instant.details.wind_speed
     }
+    private fun findWindSpeedOfGustFromData(dataLF: DataLF): Double{
+        return dataLF.instant.details.wind_speed_of_gust
+    }
+    private fun findWindDirectionFromData(dataLF: DataLF): Double{
+        return dataLF.instant.details.wind_from_direction
+    }
+
+
+
 
     override suspend fun getWindDirection(surfArea: SurfArea): List<Pair<String, Double>> {
-        return getWindData(surfArea) { dataLF -> dataLF.instant.details.wind_from_direction }
+        // Henter alle timeSeries for alle surfArea-områder
+        val allTimeSeries = SurfArea.entries.associateWith { getTimeSeries(it) }
+        // Henter timeSeries for det spesifikke surfArea-området
+        val timeSeriesForArea = allTimeSeries[surfArea]
+        // Map og konverter timeSeries-dataene til vindretning
+        return timeSeriesForArea?.map {it.first to findWindDirectionFromData(it.second)} ?: emptyList()
+
     }
 
     override suspend fun getWindSpeed(surfArea: SurfArea): List<Pair<String, Double>> {
-        return getWindData(surfArea) { dataLF -> dataLF.instant.details.wind_speed }
+        // Henter alle timeSeries for alle surfArea-områder
+        val allTimeSeries = SurfArea.entries.associateWith { getTimeSeries(it) }
+        // Henter timeSeries for det spesifikke surfArea-området
+        val timeSeriesForArea = allTimeSeries[surfArea]
+        // Map og konverter timeSeries-dataene til vindhastighet
+        return timeSeriesForArea?.map {it.first to findWindSpeedFromData(it.second)} ?: emptyList()
+
 
     }
 
     override suspend fun getWindSpeedOfGust(surfArea: SurfArea): List<Pair<String, Double>> {
-        return getWindData(surfArea) { dataLF -> dataLF.instant.details.wind_speed_of_gust }
+        // Henter alle timeSeries for alle surfArea-områder
+        val allTimeSeries = SurfArea.entries.associateWith { getTimeSeries(it) }
+        // Henter timeSeries for det spesifikke surfArea-området
+        val timeSeriesForArea = allTimeSeries[surfArea]
+        // Map og konverter timeSeries-dataene til vindretning
+        return timeSeriesForArea?.map {it.first to findWindSpeedOfGustFromData(it.second)} ?: emptyList()
+
     }
 
-    private fun inArea(lat: Double, lon: Double, surfArea: SurfArea, radius: Double = 0.1): Boolean{
-        return (
-                lat in surfArea.lat - radius..surfArea.lat + radius &&
-                lon in surfArea.lon - radius..surfArea.lon + radius
-                )
-    }
 }
