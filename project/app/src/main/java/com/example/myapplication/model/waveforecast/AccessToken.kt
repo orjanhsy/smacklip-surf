cca2473a959da9c7717a1de2e3bfc92a3110302c
orjanhsy <orjanhsy@uio.no>
2024-04-05 15:02:09 +0200

    added accesstoken data class to model

---
+++ project/app/src/main/java/com/example/myapplication/model/waveforecast/AccessToken.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.model.waveforecast
+
+import com.google.gson.annotations.SerializedName
+
+data class AccessToken (
+    @SerializedName("access_token") var accessToken: String,
+    @SerializedName("token_type")   var tokenType: String,
+    @SerializedName("expires_in")   var expiresIn: Int,
+)
