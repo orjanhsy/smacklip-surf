2553a288e2da6c474f1c3831991a11271cbb0ad4
orjanhsy <orjanhsy@uio.no>
2024-04-06 17:00:51 +0200

    update urls for waveforecast in service handler

--- project/app/src/main/java/com/example/myapplication/data/helpers/HTTPServiceHandler.kt
+++ project/app/src/main/java/com/example/myapplication/data/helpers/HTTPServiceHandler.kt
@@ -15,10 +15,23 @@ object HTTPServiceHandler {
     const val OCEAN_FORECAST_URL = "https://gw-uio.intark.uh-it.no/in2000/weatherapi/oceanforecast/2.0/complete"
 
 
-    //WaveForecast
-    const val WAVE_FORECAST_BASE = "https://www.barentswatch.no/bwapi"
-    const val WAVE_FORECAST_POINT_FORECAST = "https://www.barentswatch.no/bwapi/v1/geodata/waveforecast/pointforecasts"
-    const val WF_TEST_URL = "${WAVE_FORECAST_BASE}/v1/geodata/waveforecast/available/"
-    //returnerer tilgjengelige forecast-timestamps
-    const val WF_AVALIABLE_ALL = "${WAVE_FORECAST_BASE}/v1/geodata/waveforecast/available/all"
+    //-----------------------WaveForecast-----------------------
+    const val WF_BASE_URL = "https://www.barentswatch.no/bwapi/"
+    const val WF_ACCESS_TOKEN_URL = "https://id.barentswatch.no/connect/token"
+
+    //--------paths for WF--------
+
+    //returnerer alle tilgjengelige modeller/fairways (IKKE GJORT SERIALIZABLE) (treig)
+    const val WF_AVALIABLE_ALL_URL = "v1/geodata/waveforecast/available/all" // ? NONE
+
+    //returnerer  tilgjengelige forecast-timestamps for en fairway (IKKE GJORT SERIALIZABLE)
+    const val WF_AVALIABLE_URL = "/v1/geodata/waveforecast/available" // ?modelname&fairwayid
+
+    //returnerer bolgetilsdtander for et bestemt punkt (IKKE GJORT SERIALIZABLE)
+    const val WF_POINT_FORECAST_URL = "v1/geodata/waveforecast/pointforecast" // ?modelname&pointId&time
+
+    //returnerer alle pointforecasts tilgjenglig for et bestemt timestamp, kan hende vi skal bruke det istedefore pointforecast
+    const val WF_ALL_POINT_FORECASTS_URL = "v1/geodata/waveforecast/pointforecasts" // ?time
+
+
 }
