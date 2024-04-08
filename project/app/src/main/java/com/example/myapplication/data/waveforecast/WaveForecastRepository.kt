bed0b13c900c54d039ebe5963d06d2b6facf5134
orjanhsy <orjanhsy@uio.no>
2024-04-08 12:32:25 +0200

    add fetch timestamps function in WF with test

--- project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
@@ -1,7 +1,21 @@
 package com.example.myapplication.data.waveforecast
 
+import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.waveforecast.PointForecast
 
 interface WaveForecastRepository {
-    suspend fun getAllRelevantPointForecasts(): List<PointForecast>
+    suspend fun next3Days(): Map<SurfArea, List<PointForecast>>
 }
+
+class WaveForecastRepositoryImpl(
+    private val waveForecastDataSource: WaveForecastDataSource = WaveForecastDataSource()
+): WaveForecastRepository {
+
+    override suspend fun next3Days(): Map<SurfArea, List<PointForecast>> {
+        val avaliableForecastTimes = waveForecastDataSource.fetchAvaliableTimestamps().availableForecastTimes
+
+    }
+}
+
+
+
