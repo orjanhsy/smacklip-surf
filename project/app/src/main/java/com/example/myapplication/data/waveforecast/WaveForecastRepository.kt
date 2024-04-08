98321ac04e560a056879bea18fa1e877dcbf3625
orjanhsy <orjanhsy@uio.no>
2024-04-08 15:08:06 +0200

    add point forecast for next 3 days in WFrepo, currently only works for hoddevik

--- project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
@@ -4,16 +4,28 @@ import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.waveforecast.PointForecast
 
 interface WaveForecastRepository {
-    suspend fun next3Days(): Map<SurfArea, List<PointForecast>>
+    suspend fun next3Days(): List<Pair<String, List<PointForecast>>>
 }
 
 class WaveForecastRepositoryImpl(
     private val waveForecastDataSource: WaveForecastDataSource = WaveForecastDataSource()
 ): WaveForecastRepository {
 
-    override suspend fun next3Days(): Map<SurfArea, List<PointForecast>> {
-        val avaliableForecastTimes = waveForecastDataSource.fetchAvaliableTimestamps().availableForecastTimes
+    private fun inArea(lat: Double?, lon: Double?, surfArea: SurfArea, radius: Double = 0.7): Boolean {
+        return (
+                lat!! in surfArea.lat - radius..surfArea.lat + radius &&
+                lon!! in surfArea.lon - radius..surfArea.lon + radius
+                )
+    }
+    override suspend fun next3Days(): List<Pair<String, List<PointForecast>>> {
+        val availableForecastTimes = waveForecastDataSource.fetchAvaliableTimestamps().availableForecastTimes
+        val allPointForecastsNext3Days = availableForecastTimes.map {
+            it to waveForecastDataSource.fetchAllPointForecasts(it).filter { pointForecast ->
+                inArea(pointForecast.lat, pointForecast.lon, SurfArea.HODDEVIK)
+            }
+        }
 
+        return allPointForecastsNext3Days
     }
 }
 
