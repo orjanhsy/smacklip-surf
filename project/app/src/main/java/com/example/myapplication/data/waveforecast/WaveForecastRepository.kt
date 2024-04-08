f2d4bec2d85bb30912352e05fe628b328254fa31
orjanhsy <orjanhsy@uio.no>
2024-04-08 16:06:13 +0200

    change name of next3Days, now returns a map of forecasts for all surfareas

--- project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/waveforecast/WaveForecastRepository.kt
@@ -4,25 +4,38 @@ import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.waveforecast.PointForecast
 
 interface WaveForecastRepository {
-    suspend fun next3Days(): List<Pair<String, List<PointForecast>>>
+    suspend fun pointForecastNext3Days(): Map<String, List<List<PointForecast>>>
 }
 
 class WaveForecastRepositoryImpl(
     private val waveForecastDataSource: WaveForecastDataSource = WaveForecastDataSource()
 ): WaveForecastRepository {
 
-    private fun inArea(lat: Double?, lon: Double?, surfArea: SurfArea, radius: Double = 0.7): Boolean {
+    private fun inArea(lat: Double?, lon: Double?, surfArea: SurfArea, radius: Double = 0.1): Boolean {
         return (
                 lat!! in surfArea.lat - radius..surfArea.lat + radius &&
                 lon!! in surfArea.lon - radius..surfArea.lon + radius
                 )
     }
-    override suspend fun next3Days(): List<Pair<String, List<PointForecast>>> {
+
+    /*
+    TODO:
+    Make it faster. Unsure how to do that without hardcoding
+    - Might not have to retrieve all the data in one single call
+    */
+    override suspend fun pointForecastNext3Days(): Map<String, List<List<PointForecast>>> {
         val availableForecastTimes = waveForecastDataSource.fetchAvaliableTimestamps().availableForecastTimes
-        val allPointForecastsNext3Days = availableForecastTimes.map {
-            it to waveForecastDataSource.fetchAllPointForecasts(it).filter { pointForecast ->
-                inArea(pointForecast.lat, pointForecast.lon, SurfArea.HODDEVIK)
-            }
+
+        val allPointForecastsNext3Days = mutableMapOf<String, List<List<PointForecast>>>()
+        SurfArea.entries.map{
+            allPointForecastsNext3Days.put(
+                it.locationName,
+                availableForecastTimes.map {time ->
+                    waveForecastDataSource.fetchAllPointForecasts(time).filter { pointForecast ->
+                        inArea(pointForecast.lat, pointForecast.lon, it)
+                    }.sortedBy { pointForecast -> pointForecast.forcastDateTime}
+                }
+            )
         }
 
         return allPointForecastsNext3Days
