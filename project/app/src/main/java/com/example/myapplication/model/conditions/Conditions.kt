fe15cc5cdccf9ca5885cca4326014ab3f3747bf4
orjanhsy <orjanhsy@gmail.com>
2024-04-17 10:51:13 +0200

    add condtitionDescription enum and dictator-conditions

--- project/app/src/main/java/com/example/myapplication/model/conditions/Conditions.kt
+++ project/app/src/main/java/com/example/myapplication/model/conditions/Conditions.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication.model.conditions
 
-enum class Conditions(val description: String) {
-    GREAT("Great"),
-    DECENT("Decent"),
-    POOR("Poor")
+enum class Conditions(val value: Double) {
+    WAVE_HEIGHT_LOWER_BOUNDS(0.5),
+    WIND_SPEED_UPPER_BOUNDS(40.0),
+    WAVE_PERIOD_LOWER_BOUNDS(6.0)
 }
