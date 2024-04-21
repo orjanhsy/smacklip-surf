d0b560e551eff02ea146e4e951c38e3e6526f4f9
EvelynBizimana <evelynsb@uio.no>
2024-04-21 15:16:44 +0200

    optimized imports

--- project/app/src/main/java/com/example/myapplication/ui/home/HomeScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/home/HomeScreen.kt
@@ -40,12 +40,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
