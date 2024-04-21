cbaae816b5a3099369664db17a8b92a0e2eae3d2
orjanhsy <orjanhsy@uio.no>
2024-04-21 12:07:11 +0200

    add 'distanceTo' to metalertsRepo

--- project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
@@ -3,6 +3,11 @@ package com.example.myapplication.data.metalerts
 
 import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.metalerts.Features
+import com.mapbox.maps.extension.style.expressions.dsl.generated.distance
+import kotlin.math.PI
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 interface MetAlertsRepository{
@@ -23,11 +28,14 @@ class MetAlertsRepositoryImpl (
     }
 
 
-    private fun inArea(lat: Double, lon: Double, surfArea: SurfArea, radius: Double = 0.1): Boolean {
-        return (
-            lat in surfArea.lat - radius..surfArea.lat + radius &&
-            lon in surfArea.lon - radius..surfArea.lon + radius
-        )
+    private fun distanceTo(lat: Double, lon: Double, surfArea: SurfArea): Double {
+        // acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371 (6371 is Earth radius in km.)
+        val radiusEarth = 6371
+        val lat1 = surfArea.lat * PI / 180
+        val lon1 = surfArea.lon * PI / 180
+        val lat2 = lat * PI / 180
+        val lon2 = lon * PI / 180
+        return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2-lon1)) * radiusEarth
     }
 
     override suspend fun getRelevantAlertsFor(surfArea: SurfArea): List<Features> {
@@ -42,19 +50,19 @@ class MetAlertsRepositoryImpl (
                     i.forEach { j ->
                         val lon = j[0] as Double
                         val lat = j[1] as Double
-                        if (inArea(lat, lon, surfArea)) {
+                        if (distanceTo(lat, lon, surfArea) < 10.0) {
                             feature.let { relevantAlerts.add(it) }
                         }
                     }
                 }
-            } else if (feature.geometry?.type == "MultiPolygon") {
+            } else if (feature.geometry?.type == "MultiPolygon") { // henter alle varsel innenfor en mil
                 coordinates?.forEach { i ->
                     i.forEach { j ->
                         j.forEach { k ->
                             val coords = k as List<*>
                             val lon = coords[0] as Double
                             val lat = coords[1] as Double
-                            if (inArea(lat, lon, surfArea)) {
+                            if (distanceTo(lat, lon, surfArea) < 10.0) { // henter alle varsel innenfor en mil
                                 feature.let { relevantAlerts.add(it) }
                             }
                         }
