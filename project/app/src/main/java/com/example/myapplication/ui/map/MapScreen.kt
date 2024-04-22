a5c2b97d5a249a5ff8aed626276b690a67fafedf
munaisman <munaai@uio.no>
2024-04-22 18:58:45 +0200

    added scrolling for large cards in map

--- project/app/src/main/java/com/example/myapplication/ui/map/MapScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/map/MapScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -233,7 +235,9 @@ fun SurfAreaCard(
     ){
         Column (
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(
