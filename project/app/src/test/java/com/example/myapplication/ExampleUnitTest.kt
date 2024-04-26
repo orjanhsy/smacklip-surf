b62372ecba498205010391d9d7afbc0695384633
Agnes <agnes.birkemo@hotmail.com>
2024-04-26 11:05:49 +0200

    add common viewmodel wit data for 7 days

--- project/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ project/app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -378,7 +378,7 @@ class ExampleUnitTest {
     fun  testAsyncCalls(): Unit = runBlocking{
 
        val time = measureTimeMillis { smackLipRepository.asyncCalls().forEach{
-           println(it.value.await().size)}}
+           println(it.value.size)}}
         println(time)
 
     }
