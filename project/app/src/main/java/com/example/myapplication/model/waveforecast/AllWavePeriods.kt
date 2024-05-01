f4f04cbaf3d48e195b84a5fc847c187d531250bb
orjanhsy <orjanhsy@uio.no>
2024-05-01 13:53:27 +0200

    change updateOFLF and ui state in HSVM to type 'Map<SurfArea, DayData>'

--- project/app/src/main/java/com/example/myapplication/model/waveforecast/AllWavePeriods.kt
+++ project/app/src/main/java/com/example/myapplication/model/waveforecast/AllWavePeriods.kt
@@ -3,5 +3,5 @@ package com.example.myapplication.model.waveforecast
 import com.example.myapplication.model.surfareas.SurfArea
 
 data class AllWavePeriods(
-    val wavePeriods: Map<SurfArea, List<Double?>>
+    val wavePeriods: Map<SurfArea, List<Double?>> = mapOf()
 )
