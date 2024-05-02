9eca6cd3d188430f6e7da81abb7c93041d8f82b8
orjanhsy <orjanhsy@uio.no>
2024-05-02 12:19:30 +0200

    extensively filled out stateful Repo

--- project/app/src/main/java/com/example/myapplication/data/smackLip/Repository.kt
+++ project/app/src/main/java/com/example/myapplication/data/smackLip/Repository.kt
@@ -1,5 +1,13 @@
 package com.example.myapplication.data.smackLip
 
+import com.example.myapplication.data.locationForecast.LocationForecastRepository
+import com.example.myapplication.data.locationForecast.LocationForecastRepositoryImpl
+import com.example.myapplication.data.metalerts.MetAlertsRepository
+import com.example.myapplication.data.metalerts.MetAlertsRepositoryImpl
+import com.example.myapplication.data.oceanforecast.OceanforecastRepository
+import com.example.myapplication.data.oceanforecast.OceanforecastRepositoryImpl
+import com.example.myapplication.data.waveforecast.WaveForecastRepository
+import com.example.myapplication.data.waveforecast.WaveForecastRepositoryImpl
 import com.example.myapplication.model.metalerts.Alert
 import com.example.myapplication.model.smacklip.AllSurfAreasOFLF
 import com.example.myapplication.model.smacklip.Forecast7DaysOFLF
@@ -7,6 +15,7 @@ import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.waveforecast.AllWavePeriods
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 interface Repository {
     val ofLfNext7Days: StateFlow<AllSurfAreasOFLF>
@@ -19,16 +28,26 @@ interface Repository {
 }
 
 class RepositoryImpl(
+    val waveForecastRepository: WaveForecastRepository = WaveForecastRepositoryImpl(),
+    val oceanForecastRepository: OceanforecastRepository = OceanforecastRepositoryImpl(),
+    val locationForecastRepository: LocationForecastRepository = LocationForecastRepositoryImpl(),
+    val metAlertsRepository: MetAlertsRepository = MetAlertsRepositoryImpl()
 
 ): Repository {
 
+    init {
+        loadOFlF()
+        loadWavePeriods()
+        loadAlerts()
+    }
+
     private val _ofLfNext7Days: MutableStateFlow<AllSurfAreasOFLF> = MutableStateFlow(AllSurfAreasOFLF())
-    override val ofLfNext7Days: AllSurfAreasOFLF
-        get() = TODO("Not yet implemented")
-    override val wavePeriods: AllWavePeriods
-        get() = TODO("Not yet implemented")
-    override val alerts: Map<SurfArea, List<Alert>>
-        get() = TODO("Not yet implemented")
+    private val _wavePeriods: MutableStateFlow<AllWavePeriods> = MutableStateFlow(AllWavePeriods())
+    private val _alerts: MutableStateFlow<Map<SurfArea, List<Alert>>> = MutableStateFlow(mapOf())
+
+    override val ofLfNext7Days: StateFlow<AllSurfAreasOFLF> = _ofLfNext7Days.asStateFlow()
+    override val wavePeriods: StateFlow<AllWavePeriods> = _wavePeriods.asStateFlow()
+    override val alerts: StateFlow<Map<SurfArea, List<Alert>>> = _alerts.asStateFlow()
 
     override suspend fun loadOFlF() {
         TODO("Not yet implemented")
@@ -41,4 +60,6 @@ class RepositoryImpl(
     override suspend fun loadAlerts() {
         TODO("Not yet implemented")
     }
+
+
 }
