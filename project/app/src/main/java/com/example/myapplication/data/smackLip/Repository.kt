1b1c05711332895391490a56d2554cc0a281f226
orjanhsy <orjanhsy@uio.no>
2024-05-02 12:10:35 +0200

    add initial stateful repo

--- project/app/src/main/java/com/example/myapplication/data/smackLip/Repository.kt
+++ project/app/src/main/java/com/example/myapplication/data/smackLip/Repository.kt
@@ -5,9 +5,40 @@ import com.example.myapplication.model.smacklip.AllSurfAreasOFLF
 import com.example.myapplication.model.smacklip.Forecast7DaysOFLF
 import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.waveforecast.AllWavePeriods
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface Repository {
-    val ofLfNext7Days: AllSurfAreasOFLF
-    val wavePeriods: AllWavePeriods
-    val alerts: Map<SurfArea, List<Alert>>
+    val ofLfNext7Days: StateFlow<AllSurfAreasOFLF>
+    val wavePeriods: StateFlow<AllWavePeriods>
+    val alerts: StateFlow<Map<SurfArea, List<Alert>>>
+
+    suspend fun loadOFlF()
+    suspend fun loadWavePeriods()
+    suspend fun loadAlerts()
+}
+
+class RepositoryImpl(
+
+): Repository {
+
+    private val _ofLfNext7Days: MutableStateFlow<AllSurfAreasOFLF> = MutableStateFlow(AllSurfAreasOFLF())
+    override val ofLfNext7Days: AllSurfAreasOFLF
+        get() = TODO("Not yet implemented")
+    override val wavePeriods: AllWavePeriods
+        get() = TODO("Not yet implemented")
+    override val alerts: Map<SurfArea, List<Alert>>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun loadOFlF() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun loadWavePeriods() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun loadAlerts() {
+        TODO("Not yet implemented")
+    }
 }
