4ceb708a7971974f35379d6728439bf4adb8818c
orjanhsy <orjanhsy@uio.no>
2024-05-02 16:45:13 +0200

    add comments and tests for 'loadOFlF'

--- project/app/src/test/java/com/example/myapplication/StatefulRepoTests.kt
+++ project/app/src/test/java/com/example/myapplication/StatefulRepoTests.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import com.example.myapplication.data.smackLip.Repository
 import com.example.myapplication.data.smackLip.RepositoryImpl
+import com.example.myapplication.model.surfareas.SurfArea
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -10,10 +11,21 @@ class StatefulRepoTests {
     val repo: Repository = RepositoryImpl()
 
     @Test
-    fun testStatefulRepoOFLF() = runBlocking{
+    fun daysOfAvaliabelForecastIsX(): Unit = runBlocking{
         repo.loadOFlF()
         val state = repo.ofLfNext7Days.value
-        print(state)
+        state.next7Days.entries.map {
+            println("${it.key} -> ${it.value.forecast[it.value.forecast.size]}")
+        }
+    }
+
+    @Test
+    fun sizeOfOneDayForecast(): Unit = runBlocking {
+        repo.loadOFlF()
+        val state = repo.ofLfNext7Days.value
+        state.next7Days[SurfArea.HODDEVIK]!!.forecast.map {
+            println(it.data.size)
+        }
     }
 
 }
