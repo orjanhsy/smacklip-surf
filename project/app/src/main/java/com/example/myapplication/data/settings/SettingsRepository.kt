94b38b4eed67dee738598942f74c4db8c36152b9
Muna Abdirisak Isman <munaai@ulrik.uio.no>
2024-05-03 23:32:47 +0200

    Feature/save favorite surfareas (#220)
    * fixed conflicts
    
    * viewmodel factory for home screen
    
    * some changes to viewmodel factory
    
    * common appcontainer in homescreen
    
    * new methods for adding, removing and clearing surfarea favorites
    
    * LaunchedEffect so saved favorites appear, button to clear from data store
    
    * removed comments from proto
    
    ---------
    
    Co-authored-by: munaisman <munaai@uio.no>

--- project/app/src/main/java/com/example/myapplication/data/settings/SettingsRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/settings/SettingsRepository.kt
@@ -2,12 +2,16 @@ package com.example.myapplication.data.settings
 
 import androidx.datastore.core.DataStore
 import com.example.myapplication.Settings
+import com.example.myapplication.model.surfareas.SurfArea
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 interface SettingsRepository{
     val settingsFlow: Flow<Settings>
     suspend fun setTest(test: Double)
     suspend fun setDarkMode(enabled: Boolean)
+    suspend fun clearFavoriteSurfAreas()
+    suspend fun getFavoriteSurfAreas(): Flow<List<SurfArea>>
     suspend fun addFavoriteSurfArea(favorite: String)
     suspend fun removeFavoriteSurfArea(favorite: String)
 }
@@ -19,28 +23,54 @@ class SettingsRepositoryImpl (
         settingsStore.updateData { it.toBuilder().setTest(test).build() }
     }
 
+    override suspend fun clearFavoriteSurfAreas(){
+        settingsStore.updateData {
+            it.toBuilder().clearFavoriteSurfAreaNames().build()
+        }
+    }
+
+    override suspend fun getFavoriteSurfAreas(): Flow<List<SurfArea>> {
+        return settingsStore.data.map {
+            it.favoriteSurfAreaNamesList.mapNotNull {name ->
+                try{
+                    SurfArea.valueOf(name)
+                }catch (e: IllegalArgumentException){
+                    null
+                }
+
+            }
+        }
+    }
+
+
     override suspend fun setDarkMode(enabled: Boolean){
         settingsStore.updateData { it.toBuilder().setDarkMode(enabled).build() }
     }
 
 
+
+
     override suspend fun addFavoriteSurfArea(favorite: String){
         settingsStore.updateData {
             val builder = it.toBuilder()
-            if (!builder.favoriteSurfAreasList.contains(favorite)) {
-                val newList = builder.favoriteSurfAreasList.toMutableList().apply { add(favorite) }
-                builder.addAllFavoriteSurfAreas(newList)
-            }
+                .addFavoriteSurfAreaNames(favorite)
             builder.build()
         }
     }
 
     override suspend fun removeFavoriteSurfArea(favorite: String) {
         settingsStore.updateData {
-            val builder = it.toBuilder()
-            val newList = builder.favoriteSurfAreasList.filter { it != favorite }
-            builder.clearFavoriteSurfAreas().addAllFavoriteSurfAreas(newList)
-            builder.build()
+            val updatedList = it.favoriteSurfAreaNamesList.toMutableList().apply {
+                remove(favorite)
+            }
+            it.toBuilder()
+                .clearFavoriteSurfAreaNames()
+                .addAllFavoriteSurfAreaNames(updatedList)
+                .build()
         }
     }
 }
+
+
+
+
