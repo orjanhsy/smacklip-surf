313223f85ef1c8cd132800c85c9a766ea9bf1610
Tobias Windingstad <tobiawi@uio.no>
2024-05-10 17:52:12 +0200

    add day.month - day.month to alert

--- project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreen.kt
+++ project/app/src/main/java/com/example/myapplication/ui/surfarea/SurfAreaScreen.kt
@@ -259,8 +259,30 @@ fun SurfAreaScreen(
     }
 }
 
+fun formatTimeInterval(interval: List<String>?): String {
+    if (interval == null || interval.isEmpty()) return "Time not available"
 
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+    val firstDate = LocalDateTime.parse(interval.firstOrNull(), formatter)
+    val lastDate = LocalDateTime.parse(interval.lastOrNull(), formatter)
 
+    val firstDay = firstDate.dayOfMonth
+    val firstMonth = getMonthAbbreviation(firstDate.monthValue)
+
+    val lastDay = lastDate.dayOfMonth
+    val lastMonth = getMonthAbbreviation(lastDate.monthValue)
+
+    return "$firstDay.$firstMonth - $lastDay.$lastMonth"
+}
+
+fun getMonthAbbreviation(monthValue: Int): String {
+    val monthsAbbreviations = mapOf(
+        1 to "Jan", 2 to "Feb", 3 to "Mar", 4 to "Apr",
+        5 to "Mai", 6 to "Jun", 7 to "Jul", 8 to "Aug",
+        9 to "Sep", 10 to "Okt", 11 to "Nov", 12 to "Des"
+    )
+    return monthsAbbreviations[monthValue] ?: ""
+}
 
 @Composable
 fun ShowAlert(alerts : List<Alert>, surfArea: SurfArea, action : () -> Unit){
@@ -268,6 +290,7 @@ fun ShowAlert(alerts : List<Alert>, surfArea: SurfArea, action : () -> Unit){
     val alert = alerts.first()
     val alertMessage = alert.properties?.description ?: "No description available"
     val awarenessLevel = alert.properties?.awarenessLevel
+    val time = formatTimeInterval(alert.timeInterval?.interval)
     val icon = awarenessLevel?.let { getIconBasedOnAwarenessLevel(it) }
         ?: R.drawable.icon_awareness_default
 
@@ -276,6 +299,7 @@ fun ShowAlert(alerts : List<Alert>, surfArea: SurfArea, action : () -> Unit){
         message = alertMessage,
         actionText = "OK",
         warningIcon = icon,
+        time = time,
         data = null,
         showAlert = remember { mutableStateOf(true) },
         //actionWithValue = null,
