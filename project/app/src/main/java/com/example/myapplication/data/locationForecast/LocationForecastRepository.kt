408e9892cd2396978c038dd1e4c45bd783912dea
orjanhsy <orjanhsy@uio.no>
2024-05-11 12:45:58 +0200

    handle exception in lfRepo

--- project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/locationForecast/LocationForecastRepository.kt
@@ -13,7 +13,12 @@ class LocationForecastRepositoryImpl(
 ): LocationForecastRepository {
 
     override suspend fun getTimeSeries(surfArea: SurfArea): List<Pair<String, DataLF>> {
-        val timeSeries : List<TimeserieLF> = locationForecastDataSource.fetchLocationForecastData(surfArea).properties.timeseries
+        val timeSeries : List<TimeserieLF> = try {
+            locationForecastDataSource.fetchLocationForecastData(surfArea).properties.timeseries
+        } catch (e: Exception) {
+            // handles http exceptions thrown by data source
+            listOf()
+        }
         return timeSeries.map { it.time to it.data }
     }
 
