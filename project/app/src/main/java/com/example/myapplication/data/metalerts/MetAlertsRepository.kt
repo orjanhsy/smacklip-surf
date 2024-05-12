abe3fccea2ed4231038dc2c742b4eab80e49a24e
orjanhsy <orjanhsy@uio.no>
2024-05-12 14:01:44 +0200

    make metAlertsRepo hold a stateflow of alerts

--- project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsRepository.kt
@@ -1,20 +1,27 @@
 package com.example.myapplication.data.metalerts
 
 
+import android.util.Log
 import com.example.myapplication.model.surfareas.SurfArea
 import com.example.myapplication.model.metalerts.Alert
 import com.example.myapplication.model.metalerts.MetAlerts
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ServerResponseException
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.readText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlin.math.PI
 import kotlin.math.acos
 import kotlin.math.cos
 import kotlin.math.sin
 
+private const val TAG = "AlertRepo"
 
 interface MetAlertsRepository{
-    suspend fun loadAllRelevantAlerts(): Map<SurfArea, List<Alert>>
+    suspend fun loadAllRelevantAlerts()
 
     val alerts: StateFlow<Map<SurfArea, List<Alert>>>
 }
@@ -26,20 +33,37 @@ class MetAlertsRepositoryImpl (
 
 ) : MetAlertsRepository {
 
+    private val _allAlerts: List<Alert> = listOf()
+
+    // holds relevant alerts
     private val _alerts: MutableStateFlow<Map<SurfArea, List<Alert>>> = MutableStateFlow(mapOf())
     override val alerts: StateFlow<Map<SurfArea, List<Alert>>> = _alerts.asStateFlow()
 
-    private suspend fun loadAlerts() {
-        _allAlerts = try {
-             metAlertsDataSource.fetchMetAlertsData().features
+    private suspend fun getAlerts(): List<Alert> {
+        return try {
+            metAlertsDataSource.fetchMetAlertsData().features
+        } catch (e: ClientRequestException) {
+            val statusCode = e.response.status.value
+            val message = e.response.bodyAsText()
+            Log.e(TAG, "Client side error occurred while loading alerts. Status code: $statusCode, Message: $message")
+
+            emptyList()
+        } catch (e: ServerResponseException) {
+            val statusCode = e.response.status.value
+            val message = e.response.bodyAsText()
+            Log.e(TAG, "Server side error occurred while loading alerts. Status code: $statusCode, Message: $message")
+
+            emptyList()
         } catch (e: Exception) {
+            Log.e(TAG, "An unexpected error occurred while loading alerts: $e")
+
             emptyList()
         }
     }
 
 
+    // measures distance from a point to a surfArea (point)
     private fun distanceTo(lat: Double, lon: Double, surfArea: SurfArea): Double {
-        // acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371 (6371 is Earth radius in km.)
         val radiusEarth = 6371
         val lat1 = surfArea.lat * PI / 180
         val lon1 = surfArea.lon * PI / 180
@@ -48,13 +72,16 @@ class MetAlertsRepositoryImpl (
         return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2-lon1)) * radiusEarth
     }
 
-    override suspend fun loadAllRelevantAlerts(): Map<SurfArea, List<Alert>> {
+    override suspend fun loadAllRelevantAlerts() {
         if (_allAlerts.isEmpty()) {
-            loadAlerts()
+            getAlerts()
         }
-        return SurfArea.entries.associateWith { getRelevantAlertsFor(it) }
+        _alerts.update {
+            SurfArea.entries.associateWith { getRelevantAlertsFor(it) }
+        }
     }
 
+    // retrieves all alerts for an area that are within ALERT_RADIUS distance
     private fun getRelevantAlertsFor(surfArea: SurfArea): List<Alert> {
         val relevantAlerts: MutableList<Alert> = mutableListOf()
         _allAlerts.forEach {alert ->
@@ -64,7 +91,7 @@ class MetAlertsRepositoryImpl (
                     i.forEach { j ->
                         val lon = j[0] as Double
                         val lat = j[1] as Double
-                        if (distanceTo(lat, lon, surfArea) < ALERT_RADIUS) { // henter alle varsel innenfor en mil
+                        if (distanceTo(lat, lon, surfArea) < ALERT_RADIUS) {
                             alert.let { relevantAlerts.add(it) }
                         }
                     }
@@ -76,7 +103,7 @@ class MetAlertsRepositoryImpl (
                             val coords = k as List<*>
                             val lon = coords[0] as Double
                             val lat = coords[1] as Double
-                            if (distanceTo(lat, lon, surfArea) < ALERT_RADIUS) { // henter alle varsel innenfor en mil
+                            if (distanceTo(lat, lon, surfArea) < ALERT_RADIUS) {
                                 alert.let { relevantAlerts.add(it) }
                             }
                         }
