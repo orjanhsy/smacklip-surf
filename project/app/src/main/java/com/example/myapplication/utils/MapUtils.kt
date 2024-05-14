c04aa1bf0417678e0e989e084bc76b31efdf135d
Agnes <agnes.birkemo@hotmail.com>
2024-05-14 11:59:39 +0200

    move isMatchingCoordinates to mapUtils

--- project/app/src/main/java/com/example/myapplication/utils/MapUtils.kt
+++ project/app/src/main/java/com/example/myapplication/utils/MapUtils.kt
@@ -10,4 +10,11 @@ class MapUtils {
             Pair(it, Point.fromLngLat(it.lon, it.lat))
         }
     }
+
+    //hjelpemetode for å sjekke at to koordinater er tilnærmet like ved bruk av verdien threshold
+    fun isMatchingCoordinates(point1: Point, point2: Point): Boolean {
+        val threshold = 0.001
+        return kotlin.math.abs(point1.latitude() - point2.latitude()) <= threshold &&
+                kotlin.math.abs(point1.longitude() - point2.longitude()) <= threshold
+    }
 }
