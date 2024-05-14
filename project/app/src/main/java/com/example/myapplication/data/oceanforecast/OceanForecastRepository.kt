a73ad3b77f52d1507eaa030365bcfcfaa2988bdf
orjanhsy <orjanhsy@uio.no>
2024-05-14 18:41:16 +0200

    proper http error handling in OFREPO

--- project/app/src/main/java/com/example/myapplication/data/oceanforecast/OceanForecastRepository.kt
+++ project/app/src/main/java/com/example/myapplication/data/oceanforecast/OceanForecastRepository.kt
@@ -1,9 +1,14 @@
 package com.example.myapplication.data.oceanforecast
 
+import android.util.Log
 import com.example.myapplication.model.oceanforecast.DataOF
 import com.example.myapplication.model.oceanforecast.TimeserieOF
 import com.example.myapplication.model.surfareas.SurfArea
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.RedirectResponseException
+import io.ktor.client.plugins.ServerResponseException
 
+private const val TAG = "OFREPO"
 interface OceanForecastRepository{
     suspend fun getTimeSeries(surfArea: SurfArea): List<Pair<String, DataOF>>
 }
@@ -13,11 +18,28 @@ class OceanForecastRepositoryImpl(
 ): OceanForecastRepository {
 
     override suspend fun getTimeSeries(surfArea: SurfArea): List<Pair<String, DataOF>> {
-        //gets list of timeseries objects, containing time and data
-        val timeSeries: List<TimeserieOF> = try { dataSource.fetchOceanforecast(surfArea).properties.timeseries }
+        //gets list of timeSeries objects, containing time and data
+        val timeSeries: List<TimeserieOF> = try {
+            dataSource.fetchOceanforecast(surfArea).properties.timeseries
+        }
+        catch(e: RedirectResponseException) {
+            // 3xx
+            Log.e(TAG, "Failed get timeSeries for $surfArea. 3xx-error. Cause: ${e.message}")
+            throw e
+        }
+        catch (e: ClientRequestException) {
+            // 4xx
+            Log.e(TAG, "Failed get timeSeries for $surfArea. 4xx-error. Cause: ${e.message}")
+            throw e
+        }
+        catch(e: ServerResponseException) {
+            // 5xx
+            Log.e(TAG, "Failed get timeSeries for $surfArea. 5xx-error. Cause: ${e.message}")
+            throw e
+        }
         catch (e: Exception) {
-            // handles http exceptions from data source
-            listOf()
+            Log.e(TAG, "Failed get timeSeries for $surfArea. Unknown error. Cause: ${e.message}")
+            throw e
         }
 
         //returns a list of time mapped to data
