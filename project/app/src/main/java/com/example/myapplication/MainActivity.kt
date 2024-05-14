117e5cf5751daf5704bb2a369358fc8b28df79ec
Agnes <agnes.birkemo@hotmail.com>
2024-05-14 16:30:24 +0200

    app can be used without internet connection, snackbar is shown on the top

--- project/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ project/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.myapplication
 
 import com.example.myapplication.ui.daily.DailySurfAreaScreen
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
@@ -16,7 +20,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,12 +51,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //maximum duration of splashscreen is 10 seconds
+        var countDownFinished = false
+        object : CountDownTimer(10000, 10000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+            override fun onFinish() {
+                countDownFinished = true
+            }
+        }.start()
+
         installSplashScreen().apply {
             setKeepOnScreenCondition{
-                SmackLipApplication.container.stateFulRepo.ofLfNext7Days.value.next7Days.isEmpty()
+                SmackLipApplication.container.stateFulRepo.ofLfNext7Days.value.next7Days.isEmpty() && !countDownFinished
             }
         }
-
+        var connectetAtOnePoint = false
         val connectivityObserver = NetworkConnectivityObserver(applicationContext)
         setContent {
             val isDarkTheme by SmackLipApplication.container.infoViewModel.isDarkThemEnabled.collectAsState(initial = false)
@@ -62,33 +83,42 @@ class MainActivity : ComponentActivity() {
                 ) {
                     if (isConnected) {
                         SmackLipNavigation()
+                        connectetAtOnePoint = true
                     }else{
-                        ShowSnackBar()
-                        if (isConnected) {
-                            SmackLipNavigation()
+                        if (connectetAtOnePoint){
+                            showSnackBarAndNavigation()
+                        }else{
+                            ShowSnackBar()
                         }
                     }
                 }
-
             }
         }
     }
-
 }
 
+@Composable
+fun showSnackBarAndNavigation(){
+    Column(){
+        Row{
+            ShowSnackBar()
+        }
+        Row{
+            SmackLipNavigation()
+        }
 
+    }
+}
 @Composable
 fun ShowSnackBar() {
     Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)){
+        .fillMaxWidth()){
         Snackbar(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier.padding(8.dp),
         ) {
             Text(text = "Vennligst koble til internett.")
         }
     }
-
 }
 
 @Composable
