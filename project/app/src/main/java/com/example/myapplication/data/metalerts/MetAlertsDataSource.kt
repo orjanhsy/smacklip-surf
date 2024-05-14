a554e8345008b81d5a054f6753e4f36a239175c6
orjanhsy <orjanhsy@uio.no>
2024-05-14 18:57:34 +0200

    add explicit exception handling in metalertsDS

--- project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsDataSource.kt
+++ project/app/src/main/java/com/example/myapplication/data/metalerts/MetAlertsDataSource.kt
@@ -5,6 +5,9 @@ import com.example.myapplication.utils.HTTPServiceHandler.METALERTS_URL
 import com.example.myapplication.model.metalerts.MetAlerts
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.RedirectResponseException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
@@ -20,14 +23,29 @@ class MetAlertsDataSource(private val metAlertsUrl: String = METALERTS_URL) {
     }
 
 
+    // gets all currently available alerts from MET
     suspend fun fetchMetAlertsData(): MetAlerts {
-        val response = try {
-            client.get(metAlertsUrl)
-        } catch (e: Exception) {
-            // does not handle exceptions differently
-            Log.e(TAG, "${e.message}")
+        return try {
+            client.get(metAlertsUrl).body()
+        }
+        catch(e: RedirectResponseException) {
+            // 3xx
+            Log.e(TAG, "Failed get alerts. 3xx-error. Cause: ${e.message}")
             throw e
         }
-        return response.body()
+        catch (e: ClientRequestException) {
+            // 4xx
+            Log.e(TAG, "Failed get alerts. 4xx-error. Cause: ${e.message}")
+            throw e
+        }
+        catch(e: ServerResponseException) {
+            // 5xx
+            Log.e(TAG, "Failed get alerts. 5xx-error. Cause: ${e.message}")
+            throw e
+        }
+        catch (e: Exception) {
+            Log.e(TAG, "Failed get alerts. Unknown error. Cause: ${e.message}")
+            throw e
+        }
     }
 }
