405bdccc48f118ac0b16ab388c00a778434d6a87
Agnes <agnes.birkemo@hotmail.com>
2024-05-14 12:43:27 +0200

    translate comments to english

--- project/app/src/main/java/com/example/myapplication/utils/MapUtils.kt
+++ project/app/src/main/java/com/example/myapplication/utils/MapUtils.kt
@@ -5,13 +5,14 @@ import com.mapbox.geojson.Point
 
 class MapUtils {
 
+    //get a list mapping surf areas to the respective point based on the coorrdinates
     fun locationToPoint(): List<Pair<SurfArea, Point>> {
         return SurfArea.entries.map {
             Pair(it, Point.fromLngLat(it.lon, it.lat))
         }
     }
 
-    //hjelpemetode for å sjekke at to koordinater er tilnærmet like ved bruk av verdien threshold
+    //Helper method to check if two coordinates are approximately equal using the value of threshold.
     fun isMatchingCoordinates(point1: Point, point2: Point): Boolean {
         val threshold = 0.001
         return kotlin.math.abs(point1.latitude() - point2.latitude()) <= threshold &&
