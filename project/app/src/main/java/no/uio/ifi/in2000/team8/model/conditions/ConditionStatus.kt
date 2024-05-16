c603386041495dff1ef980c0bde89543237c51ec
juliesmac <julikal@uio.no>
2024-05-16 17:55:49 +0200

    Updated until daily

--- project/app/src/main/java/no/uio/ifi/in2000/team8/model/conditions/ConditionStatus.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/team8/model/conditions/ConditionStatus.kt
@@ -3,8 +3,8 @@ package no.uio.ifi.in2000.team8.model.conditions
 import no.uio.ifi.in2000.team8.R
 
 enum class ConditionStatus(val description: String, val surfBoard: Int, val value: Int) {
-    GREAT("Utmerket", R.drawable.greenboard, 0),
-    DECENT("Greit", R.drawable.yellowboard, 1),
-    POOR("Dårlig", R.drawable.redboard, 2),
-    BLANK("", R.drawable.blankboard, 3)
+    GREAT("Utmerket", R.drawable.grønt_brett, 0),
+    DECENT("Greit", R.drawable.gult_brett, 1),
+    POOR("Dårlig", R.drawable.rødt_brett, 2),
+    BLANK("", R.drawable.gjennomsiktig_brett, 3)
 }
