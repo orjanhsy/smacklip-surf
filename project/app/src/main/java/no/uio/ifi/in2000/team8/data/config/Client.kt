de6427807918bad1538c8c27e63215b8af4ac0ce
orjanhsy <orjanhsy@uio.no>
2024-05-16 21:14:24 +0200

    add clarifying comment in client file

--- project/app/src/main/java/no/uio/ifi/in2000/team8/data/config/Client.kt
+++ project/app/src/main/java/no/uio/ifi/in2000/team8/data/config/Client.kt
@@ -1,5 +1,6 @@
 package no.uio.ifi.in2000.team8.data.config
-
+// used to authorize access to barentswatch (waveforecast)
+// will be removed (also from git log) before making the git repository public
 object Client {
     const val CLIENT_ID = "orjanhsy@uio.no:smacklip"
     const val CLIENT_SECRET = "agnestestfil"
